ccc4c1cdc47a7aad990b60d6c36e528912aeba06
Bohdan <kalaczynskibo@gmail.com>
2023-10-23 15:37:07 +0300

    changed employees

--- src/forProject/Employees.java
+++ src/forProject/Employees.java
@@ -3,10 +3,12 @@ package forProject;
 public class Employees {
     private String position;
     private int countOfStaff;
+    private String stringi;
 
-    public Employees(String position, int countOfStaff) {
+    public Employees(String position, int countOfStaff, String stringi) {
         this.position = position;
         this.countOfStaff = countOfStaff;
+        this.stringi = stringi;
     }
 
     public void makingHisJob(){
